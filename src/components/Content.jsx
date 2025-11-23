82b6b903b984721cb08480e59b754b83f3e3a97a
fredieposh <maor1234@gmail.com>
2025-11-24 00:26:59 +0200

    Style home component

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -1,10 +1,17 @@
+import Home from "./Home.jsx"
+
 export default function Content() {
     return (
         <div 
+        className="page-container"
         style={{
             display: "flex",
             flex: 16,
+            flexDirection: "column",
+            alignItems: "center",
+            justifyContent: "center",
         }}>
+            <Home />
         </div>
     )
 }
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,11 +1,17 @@
 import { Link } from 'react-router'
+import './Home.css'
 
 export default function Home() {
     return(
         <div className="home">
             <div className="welcome-banner">
-                <h2>Terminal Y</h2>
-                <h3>All The The Finest Brands In One Place</h3>
+                <div>
+
+                </div>
+                <div>
+                    <h2>Terminal Y</h2>
+                    <h3>All The The Finest Brands In One Place</h3>
+                </div>
             </div>
             <div className="info-banner">
                 <p>Just a taste of the brands you can find here:</p>
