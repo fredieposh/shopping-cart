977cb7af45cf64aae4bbe60b29fd1412628357eb
fredieposh <maor1234@gmail.com>
2025-11-23 21:40:45 +0200

    Create basic content compnent

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
 import Navbar from './components/Navbar.jsx';
+import Content from './components/Content.jsx';
 import './App.css';
 
 function App() {
   return (
-    <Navbar />
+    <>
+      <Navbar />
+      <Content />    
+    </>
   );
 };
 
---
+++ src/components/Content.jsx
@@ -0,0 +1,10 @@
+export default function Content() {
+    return (
+        <div 
+        style={{
+            display: "flex",
+            flex: 16,
+        }}>
+        </div>
+    )
+}
